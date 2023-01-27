d96061409e83b8b4973a59324dc5e6c2ff86b319
mulengangulale <95334991+mulengangulale@users.noreply.github.com>
2023-01-27 11:41:35 -0500

    Third Commit

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 var gamePattern = []
-var randomChosenColor = nextSequence()
-gamePattern.push(randomChosenColor)
+var userClickedPattern = []
+// nextSequence()
 
-// Adding sound and making the button flash
-$("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-$("#"+randomChosenColor).click(function(){
-    var sound = new Audio (`sounds/${randomChosenColor}.mp3`);
-    sound.play()
-})
 
-// Choosing a random color
+// Storing the id of the button clicked in an array and playing sound of clicked button
+$(".btn").on("click", function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor)
+    // console.log(userClickedPattern)
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+// Plays sound of clicked button
+function playSound(name){
+    // $("#"+name).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    // $("#"+name).click(function(){
+        var sound = new Audio (`sounds/${name}.mp3`);
+        sound.play()
+    // });
+}
+
+// Choosing a random color and making Sound when a button is clicked
 function nextSequence() {
     var number = Math.floor(Math.random()*4);
     var buttonColors = ["red", "blue", "green", "yellow"]
-    var color = buttonColors[number]
-    return color
+    var randomChosenColor = buttonColors[number]
+    gamePattern.push(randomChosenColor)
+    var sound = new Audio (`sounds/${randomChosenColor}.mp3`);
+    sound.play()
+
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor)
+    // $("#"+randomChosenColor).click(function(event){
+        // var sound = new Audio (`sounds/${randomChosenColor}.mp3`);
+        // sound.play()
+    // });
+
+}
+
+// Adding some background effects when button is pressed
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed")
+    setTimeout(function () { 
+        $("#"+currentColor).removeClass("pressed");
+    }, 70);
 }
